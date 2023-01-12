38b1a1a4418e560af6f5a98a824efc0ec9a8573e
Cristian Rodriguez <an1me@hotmail.es>
2023-01-11 20:19:12 -0500

    task-02.js, completado

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
+"use strict";
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const selec = document.querySelector("#ingredients");
+
+ingredients.forEach((element) => {
+  const elemento = document.createElement("li");
+  elemento.textContent = element;
+  elemento.classList.add("item");
+  selec.appendChild(elemento);
+});
