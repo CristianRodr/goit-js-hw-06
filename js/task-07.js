10b09f74c995e8c6d45337d21dcb055b52a5aa4b
Cristian Rodriguez <an1me@hotmail.es>
2023-01-14 18:30:26 -0500

    task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,1 +1,8 @@
+const selecInput = document.querySelector("input");
 
+const text = document.querySelector("#text");
+
+selecInput.addEventListener("click", (event) => {
+  let tomaRango = event.target.value;
+  text.style.fontSize = `${tomaRango}px`;
+});
