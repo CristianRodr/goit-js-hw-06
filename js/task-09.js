bfd025dc70a0f0ae04b393d58d9f0a4864b1107f
Cristian Rodriguez <an1me@hotmail.es>
2023-01-15 13:41:11 -0500

    task-09.js, completado

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,24 @@
+//funcion color aleatorio_____________________________________________
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+//____________________________________________________________________
+
+const textColor = document.querySelector(".color");
+
+//-------------------------------------------------------------------
+const fondoBody = document.querySelector("body");
+
+function fondoFuncion(fondoBody) {
+  //fondoBody.style.backgroundColor = getRandomHexColor();
+  textColor.textContent = fondoBody.style.backgroundColor = getRandomHexColor();
+}
+//--------------------------------------------------------------------
+
+const button = document.querySelector(".change-color");
+
+const handleClick = () => {
+  fondoFuncion(fondoBody);
+};
+
+button.addEventListener("click", handleClick);
